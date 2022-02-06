441a2d97f5bf2dfc39bf494eeb62e09f89b519e4
pgananc <56308029+pgananc@users.noreply.github.com>
2022-02-05 22:04:31 -0500

    No testing

--- feign/department-service/src/test/java/com/example/pageable/MigrationDataFlywayApplicationTests.java
+++ feign/department-service/src/test/java/com/example/pageable/MigrationDataFlywayApplicationTests.java
@@ -13,25 +13,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class MigrationDataFlywayApplicationTests {
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Test
-	public void dada_migracion_entonces_ids_no_deben_estar_nulos() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/employees-migration").accept(MediaType.APPLICATION_JSON))
-				.andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.*").exists())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.[*].id").isNotEmpty())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.[0].firstName").value("pepito"));
-	}
-
-	@Test
-	public void dada_migracion_entonces_nombre_primer_registro_es_pepito() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/employees-migration").accept(MediaType.APPLICATION_JSON))
-				.andDo(print()).andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.[0].firstName").value("pepito"));
-	}
 
 }
